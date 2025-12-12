9a2cb02ac18f62bad76b971b6e0c3df790f8a862
mgutierrez1477 <mgutierrez1477@gmail.com>
2025-12-11 21:42:00 -0600

    Todo listo

--- build.sh
+++ build.sh
@@ -5,7 +5,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# Crear superuser automáticamente si las vars están presentes.
-# Django crea el usuario cuando existen DJANGO_SUPERUSER_* en env y usamos --no-input.
 python manage.py createsuperuser --no-input || true
--- gestion_tareas/settings.py
+++ gestion_tareas/settings.py
@@ -74,10 +74,7 @@ WSGI_APPLICATION = 'gestion_tareas.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default="sqlite:///db.sqlite3", conn_max_age=600)
 }
 
 
